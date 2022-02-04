c0792005f56595ccaf87f1321d7d9bbe1022db2e
deniskeller <dentean@mail.ru>
2022-02-04 20:11:39 +0400

    ref

--- pages/app/latest_updates.tsx
+++ pages/app/latest_updates.tsx
@@ -1,1 +1,15 @@
-export {};
+// import type { NextPage } from 'next';
+// import styles from './LatestUpdatesApp.module.scss';
+
+import { Application } from '@layouts/index';
+import { HomeApp } from '@view/index';
+
+const LatestUpdatesApp = () => {
+  return (
+    <Application>
+      <HomeApp />
+    </Application>
+  );
+};
+
+export default LatestUpdatesApp;
