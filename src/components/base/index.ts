d14cb09b97125c999b280c5c8145dfdb695bff1a
deniskeller <dentean@mail.ru>
2022-02-08 00:05:01 +0400

    1st widget, basecheckbox

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -8,3 +8,4 @@ export { default as BaseSubtitle } from './BaseSubtitle/BaseSubtitle';
 export { default as BaseText } from './BaseText/BaseText';
 export { default as BaseSelect } from './BaseSelect/BaseSelect';
 export { default as BaseSearchSelect } from './BaseSearchSelect/BaseSearchSelect';
+export { default as BaseCheckbox } from './BaseCheckbox/BaseCheckbox';
--- src/components/content/widgets/Widget/Widget.tsx
+++ src/components/content/widgets/Widget/Widget.tsx
@@ -1,4 +1,10 @@
-import { BaseInput, BaseSelect, BaseButton, BaseIcon } from '@base/index';
+import {
+  BaseInput,
+  BaseSelect,
+  BaseButton,
+  BaseIcon,
+  BaseCheckbox,
+} from '@base/index';
 import { ALL_ICONS } from '@constants/icons';
 import useOnClickOutside from '@hooks/useOnClickOutside';
 import React from 'react';
@@ -31,12 +37,9 @@ const Widget: React.FC<Props> = ({}) => {
   const [isOpen, setIsOpen] = React.useState(false);
   const [selectedOption, setSelectedOption] = React.useState('');
   const selectContainerRef = React.useRef(null);
-
   const clickOutsideHandler = () => setIsOpen(false);
   useOnClickOutside(selectContainerRef, clickOutsideHandler);
-
   const toggling = () => setIsOpen(!isOpen);
-
   const onOptionClicked = (value: string) => () => {
     setSelectedOption(value);
     setIsOpen(false);
@@ -53,10 +56,21 @@ const Widget: React.FC<Props> = ({}) => {
     e.preventDefault();
   };
 
+  //логика чекбокса
+  const [checkboxValue, setCheckboxValue] = React.useState<boolean>(true);
+  const changeHandlerCheckbox = (value: boolean) => {
+    setCheckboxValue(value);
+  };
+
+  React.useEffect(() => {
+    console.log('checkboxValue: ', checkboxValue);
+  }, [checkboxValue]);
+
   return (
     <>
       <div className={styles.Widget}>
-        {/* <BaseInput
+        <div className={styles.Widgetform}>
+          {/* <BaseInput
         name='companyName'
         placeholder='Your Company Name'
         type='text'
@@ -76,74 +90,75 @@ const Widget: React.FC<Props> = ({}) => {
         Check
       </BaseButton> */}
 
-        <div className={styles.BaseInput}>
-          <input
-            value={companyName}
-            type='text'
-            className={`${styles.Input} ${error ? styles.Error : ''}`}
-            name='companyName'
-            placeholder='Your Company Name'
-            required={true}
-            autoComplete='true'
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              changeHandlerCompanyName(e.target.value)
-            }
-          />
-          {error ? <div className={styles.ErrorText}>{error}</div> : ''}
-        </div>
-
-        <div className={styles.SelectContainer} ref={selectContainerRef}>
-          <div
-            className={`${styles.SelectHeader}  ${
-              isOpen ? styles.SelectHeaderFocus : ''
-            }`}
-            onClick={toggling}
-          >
-            <span className={`${selectedOption ? styles.NotEmpty : ''}`}>
-              {selectedOption || 'Selected Juristdiction'}
-            </span>
-            <BaseIcon
-              icon={ALL_ICONS.SELECT_ARROW}
-              viewBox='0 0 16 16'
-              className={`${styles.IconArrow} ${styles.IconArrowTop}`}
+          <div className={styles.BaseInput}>
+            <input
+              value={companyName}
+              type='text'
+              className={`${styles.Input} ${error ? styles.Error : ''}`}
+              name='companyName'
+              placeholder='Your Company Name'
+              required={true}
+              autoComplete='true'
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                changeHandlerCompanyName(e.target.value)
+              }
             />
-            <BaseIcon
-              icon={ALL_ICONS.SELECT_ARROW}
-              viewBox='0 0 16 16'
-              className={`${styles.IconArrow} ${styles.IconArrowBottom}`}
-            />
+            {error ? <div className={styles.ErrorText}>{error}</div> : ''}
           </div>
-          {isOpen && (
-            <ul className={styles.SelectList}>
-              {options.map((option: ISelectItem) => (
-                <li
-                  className={styles.ListItem}
-                  onClick={onOptionClicked(option.title)}
-                  key={Math.random()}
-                >
-                  <span className={styles.ListItemTitle}>{option.title}</span>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
 
-        <div className={styles.BaseButton}>
-          <button
-            disabled={false}
-            className={styles.Button}
-            onClick={submitFormData}
-          >
-            Check
-          </button>
+          <div className={styles.SelectContainer} ref={selectContainerRef}>
+            <div
+              className={`${styles.SelectHeader}  ${
+                isOpen ? styles.SelectHeaderFocus : ''
+              }`}
+              onClick={toggling}
+            >
+              <span className={`${selectedOption ? styles.NotEmpty : ''}`}>
+                {selectedOption || 'Selected Juristdiction'}
+              </span>
+              <BaseIcon
+                icon={ALL_ICONS.SELECT_ARROW}
+                viewBox='0 0 16 16'
+                className={`${styles.IconArrow} ${styles.IconArrowTop}`}
+              />
+              <BaseIcon
+                icon={ALL_ICONS.SELECT_ARROW}
+                viewBox='0 0 16 16'
+                className={`${styles.IconArrow} ${styles.IconArrowBottom}`}
+              />
+            </div>
+            {isOpen && (
+              <ul className={styles.SelectList}>
+                {options.map((option: ISelectItem) => (
+                  <li
+                    className={styles.ListItem}
+                    onClick={onOptionClicked(option.title)}
+                    key={Math.random()}
+                  >
+                    <span className={styles.ListItemTitle}>{option.title}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <div className={styles.BaseButton}>
+            <button
+              disabled={false}
+              className={styles.Button}
+              onClick={submitFormData}
+            >
+              Check
+            </button>
+          </div>
         </div>
+        <BaseCheckbox
+          onClick={changeHandlerCheckbox}
+          checkboxValue={checkboxValue}
+        >
+          Show only ready-made companies
+        </BaseCheckbox>
       </div>
-      <input
-        type='checkbox'
-        id='subscribeNews'
-        name='subscribe'
-        value='newsletter'
-      ></input>
     </>
   );
 };
