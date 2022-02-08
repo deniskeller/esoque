a4885596d156b88b80f394c4394ce0bd443d6573
deniskeller <dentean@mail.ru>
2022-02-08 22:30:07 +0400

    ref, footer  bg type

--- src/components/footer/Footer/Footer.tsx
+++ src/components/footer/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import { BaseContainer, BaseIcon } from '@base/index';
 import { ALL_ICONS } from '@constants/icons';
 import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
 import styles from './Footer.module.scss';
 
 const links = [
@@ -17,44 +18,60 @@ const links = [
   { href: '/', title: 'News & Insights' },
 ];
 
-const Footer: React.FC = () => {
+const Footer: React.FC = ({}) => {
+  const router = useRouter();
+  const [type, setType] = React.useState('');
+
+  React.useEffect(() => {
+    if (router.pathname == '/esoque') {
+      setType('light');
+    }
+  }, [router.pathname]);
+
   return (
     <>
-      <BaseContainer>
-        <div className={styles.Footer}>
-          <div className={styles.FooterLogoContent}>
-            <div className={styles.FooterLogoContentTop}>
-              <BaseIcon
-                icon={ALL_ICONS.LOGO_TEXT}
-                viewBox='0 0 290 22'
-                className={styles.LogoText}
-              />
-              <BaseIcon
-                icon={ALL_ICONS.LOGO}
-                viewBox='0 0 440 70'
-                className={styles.Logo}
-              />
+      <div
+        className={`${styles.FooterWrapper} ${
+          type == 'light' ? styles.Light : ''
+        }`}
+      >
+        {type}
+        <BaseContainer>
+          <div className={styles.Footer}>
+            <div className={styles.FooterLogoContent}>
+              <div className={styles.FooterLogoContentTop}>
+                <BaseIcon
+                  icon={ALL_ICONS.LOGO_TEXT}
+                  viewBox='0 0 290 22'
+                  className={styles.LogoText}
+                />
+                <BaseIcon
+                  icon={ALL_ICONS.LOGO}
+                  viewBox='0 0 440 70'
+                  className={styles.Logo}
+                />
+              </div>
+              <div className={styles.FooterLogoContentBottom}>
+                <BaseIcon
+                  icon={ALL_ICONS.LOGO_ESOQUE}
+                  viewBox='0 0 174 17'
+                  className={styles.LogoEsoque}
+                />
+              </div>
             </div>
-            <div className={styles.FooterLogoContentBottom}>
-              <BaseIcon
-                icon={ALL_ICONS.LOGO_ESOQUE}
-                viewBox='0 0 174 17'
-                className={styles.LogoEsoque}
-              />
+            <div className={styles.FooterNavbar}>
+              {links &&
+                links.map((item, idex) => {
+                  return (
+                    <Link href={`${item.href}`} key={idex}>
+                      <a className={styles.Link}>{item.title}</a>
+                    </Link>
+                  );
+                })}
             </div>
           </div>
-          <div className={styles.FooterNavbar}>
-            {links &&
-              links.map((item, idex) => {
-                return (
-                  <Link href={`${item.href}`} key={idex}>
-                    <a className={styles.Link}>{item.title}</a>
-                  </Link>
-                );
-              })}
-          </div>
-        </div>
-      </BaseContainer>
+        </BaseContainer>
+      </div>
     </>
   );
 };
